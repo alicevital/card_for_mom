1e81c576b8515f42815b0397b8709605617c04a1
Alice Emily <alice.nascimento@bsatech.io>
2026-05-16 17:58:02 -0300

    feat: refactor message generation logic and integrate API client

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
 
 app = FastAPI()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,33 +26,37 @@ class MessageRequest(BaseModel):
     traits: str
     memory: str
 
+print("API KEY:", os.getenv("GEMINI_API_KEY"))
+
 
 @app.post("/generate")
 async def generate_message(data: MessageRequest):
+    try:
+        prompt = f"""
+        Escreva uma mensagem curta e emocional para o Dia das Mães.
 
-    if len(data.memory) > 200:
-        return {"message": "Texto muito longo. Reduza a memória."}
+        Nome da mãe: {data.motherName}
+        Autor: {data.userName}
+        Características: {data.traits}
+        Memória com a mãe: {data.memory}
 
-    if len(data.traits) > 100:
-        return {"message": "Características muito longas."}
-    
-    message = f"""
-    Escreva uma mensagem curta e emocional para o Dia das Mães.
+        Regras:
+        - máximo 300 caracteres
+        - não inventar informações
+        - escrever em português
+        """
 
-    Use APENAS as informações abaixo:
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=prompt
+        )
 
-    Nome da mãe: {data.motherName}
-    Autor: {data.userName}
-    Características: {data.traits}
-    Memória com a mãe: {data.memory}
+        return {
+            "message": response.text
+        }
 
-    Regras:
-    - Máximo 300 caracteres
-    - Não inventar informações
-    - Escrever em português
-    """
-
-    #simulação
-    generated = f"{data.motherName}, você é {data.traits}. Nunca esqueço {data.memory}. Com amor, {data.userName}"
-
-    return {"message": generated}
+    except Exception as e:
+        print("ERRO:", e)
+        return {
+            "message": "Erro ao gerar mensagem!!!!!!!!!!!!"
+        }
